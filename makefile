4713269e56060411cfcabc69b87c1fced6675a3a
N1ghtStorm <bushuevdv@mail.ru>
2021-10-04 10:57:57 +0300

    initial commit

---
+++ makefile
@@ -0,0 +1,11 @@
+run-tmp:
+	SKIP_WASM_BUILD= cargo run --release -- --dev --tmp
+
+run:
+	SKIP_WASM_BUILD= cargo run --release
+
+build:
+	SKIP_WASM_BUILD= cargo build --release
+
+test:
+	SKIP_WASM_BUILD= cargo test
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::{
+    ensure,
+    decl_error, 
+    decl_module, 
+    decl_storage,
+    dispatch::{
+        DispatchResult, 
+        DispatchError, 
+        Vec,
+    },
+};
+use frame_system::{
+    ensure_signed,
+};
+
+use frame_support::traits::IntegrityTest;
+
+use frame_support::sp_std::{
+    cmp::{
+        Eq, 
+        PartialEq}, 
+};
+
+#[cfg(test)]
+pub mod mock;
+
+#[cfg(test)]    
+pub mod tests;
