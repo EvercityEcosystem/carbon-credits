fe58fc427e94a7385681dc69eeeab67f0580abfe
N1ghtStorm <bushuevdv@mail.ru>
2021-10-11 13:19:03 +0300

    add not owner test

--- src/tests/project_tests.rs
+++ src/tests/project_tests.rs
@@ -5,11 +5,12 @@ use frame_support::{assert_ok, dispatch::{
 }};
 use crate::H256;
 use crate::standard::Standard;
+use pallet_evercity_accounts::accounts::*;
 
 
 #[test]
 fn it_works_get_unexisting_project() {
-	new_test_ext().execute_with(|| {
+        new_test_ext().execute_with(|| {
                 let owner = ROLES[1].0;
                 let standard = Standard::default();
                 let filehash = H256::from([0x66; 32]);
@@ -17,7 +18,7 @@ fn it_works_get_unexisting_project() {
                 let option = CarbonCredits::get_proj_by_id(2);
 
                 assert!(option.is_none())
-	});
+        });
 }
 
 #[test]
@@ -61,7 +62,6 @@ fn it_works_for_full_cycle_sign_project_gold_standard() {
                 let standard_acc = ROLES[3].0;
                 let registry = ROLES[5].0;
 
-
                 let standard = Standard::GoldStandard;
                 let filehash = H256::from([0x66; 32]);
 
@@ -76,7 +76,6 @@ fn it_works_for_full_cycle_sign_project_gold_standard() {
                 let project_after_standard_sign = CarbonCredits::get_proj_by_id(1).unwrap();
                 let registry_acc_sign_result = CarbonCredits::sign_project(Origin::signed(registry), 1);
                 let project_after_registry_sign = CarbonCredits::get_proj_by_id(1).unwrap();
-
 
                 assert_ok!(owner_sign_result, ());
                 assert_ok!(auditor_sign_result, ());
@@ -97,12 +96,49 @@ fn it_works_for_full_cycle_sign_project_gold_standard() {
                 assert!(project_after_registry_sign.signatures.iter().any(|x| *x == auditor));
                 assert!(project_after_registry_sign.signatures.iter().any(|x| *x == standard_acc));
                 assert!(project_after_registry_sign.signatures.iter().any(|x| *x == registry));
+
+                assert_eq!(project_after_registry_sign.standard, Standard::GoldStandard);
+                assert_eq!(1, project_after_registry_sign.document_versions.len());
+                assert_eq!(project_after_registry_sign.document_versions[0].filehash, filehash);
 	});
 }
 
-// #[test]
-// fn it_works_for_owner_sign_project_gold_standard() {
-// 	new_test_ext().execute_with(|| {
+#[test]
+fn it_fails_sign_project_gold_standard_not_an_owner() {
+	new_test_ext().execute_with(|| {
+                let owner = ROLES[1].0;
+                let standard = Standard::GoldStandard;
+                let filehash = H256::from([0x66; 32]);
 
-// 	});
-// }
+                let _ = CarbonCredits::create_project(Origin::signed(owner), standard, filehash);
+
+                ROLES.iter()
+                     .filter(|x| x.1 != CC_PROJECT_OWNER_ROLE_MASK)
+                     .map(|x| x.0)
+                     .for_each(|x| {
+                        let owner_sign_result = CarbonCredits::sign_project(Origin::signed(x), 1);
+                        assert_ne!(owner_sign_result, DispatchResult::Ok(()));
+                });
+	});
+}
+
+#[test]
+fn it_fails_sign_project_gold_standard_not_an_auditor() {
+	new_test_ext().execute_with(|| {
+                todo!();
+	});
+}
+
+#[test]
+fn it_fails_sign_project_gold_standard_not_a_standard_acc() {
+	new_test_ext().execute_with(|| {
+                todo!();
+	});
+}
+
+#[test]
+fn it_fails_sign_project_gold_standard_not_a_registry() {
+	new_test_ext().execute_with(|| {
+                todo!();
+	});
+}
