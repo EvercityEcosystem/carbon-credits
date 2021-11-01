eaba4d6c3a81d61502a09d78730acfee92c79e6a
N1ghtStorm <bushuevdv@mail.ru>
2021-11-01 18:51:16 +0300

    commend carbon credit mod

--- src/carbon_credits.rs
+++ src/carbon_credits.rs
@@ -1,14 +1,14 @@
-use crate::standard::Standard;
-use crate::project::ProjectId;
+// use crate::standard::Standard;
+// use crate::project::ProjectId;
 
-pub struct CarbonCreditBatch<Moment> where Moment: pallet_timestamp::Config {
-    pub standard: Standard,
-    pub project_id: ProjectId, 
-    pub timestamp: Moment,
-    pub count: u64,
-}
+// pub struct CarbonCreditBatch<Moment> where Moment: pallet_timestamp::Config {
+//     pub standard: Standard,
+//     pub project_id: ProjectId, 
+//     pub timestamp: Moment,
+//     pub count: u64,
+// }
 
-pub struct CarbonCreditWarehouse<AccountId, Moment> where Moment: pallet_timestamp::Config {
-    pub owner: AccountId,
-    pub carbon_credit_batches: Vec<CarbonCreditBatch<Moment>>,
-}
+// pub struct CarbonCreditWarehouse<AccountId, Moment> where Moment: pallet_timestamp::Config {
+//     pub owner: AccountId,
+//     pub carbon_credit_batches: Vec<CarbonCreditBatch<Moment>>,
+// }
