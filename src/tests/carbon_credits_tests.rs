be2032c04743f12a642d6638e913c0f5cc6d388e
N1ghtStorm <bushuevdv@mail.ru>
2021-11-05 14:10:18 +0300

    add tests

--- src/tests/carbon_credits_tests.rs
+++ src/tests/carbon_credits_tests.rs
@@ -12,11 +12,35 @@ use crate::tests::helpers::*;
 
 
 #[test]
-fn it_works_for_create_new_annual_report_gold_standard() {
+fn it_works_for_create_new_cc_gold_standard() {
     new_test_ext().execute_with(|| {
         let (_, project_id, owner) = get_registerd_project_and_owner_gold_standard();
         full_sign_annual_report_gold_standard();
         let create_cc_result = CarbonCredits::create_carbon_credits(Origin::signed(owner), 1, owner, 1, project_id);
         assert_ok!(create_cc_result, ());
+    });
+}
+
+#[test]
+fn it_fails_for_create_cc_asset_not_owner_role() {
+    new_test_ext().execute_with(|| {
+        let (_, project_id, owner) = get_registerd_project_and_owner_gold_standard();
+        full_sign_annual_report_gold_standard();
+
+        let _ = EvercityAccounts::account_withdraw_role(Origin::signed(ROLES[0].0), owner, CC_PROJECT_OWNER_ROLE_MASK);
+        let create_cc_result = CarbonCredits::create_carbon_credits(Origin::signed(owner), 1, owner, 1, project_id);
+        assert_ne!(create_cc_result, Ok(()));
+    });
+}
+
+#[test]
+fn it_fails_for_create_cc_asset_not_owner_account() {
+    new_test_ext().execute_with(|| {
+        let (_, project_id, _) = get_registerd_project_and_owner_gold_standard();
+        full_sign_annual_report_gold_standard();
+        let new_owner_id = 555;
+        let _ = EvercityAccounts::account_add_with_role_and_data(Origin::signed(ROLES[0].0), new_owner_id, CC_PROJECT_OWNER_ROLE_MASK);
+        let create_cc_result = CarbonCredits::create_carbon_credits(Origin::signed(new_owner_id), 1, new_owner_id, 1, project_id);
+        assert_ne!(create_cc_result, Ok(()));
     });
 }
