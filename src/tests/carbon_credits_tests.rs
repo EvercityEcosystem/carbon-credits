20f408d7b7b4859f3b09a9e89896d734766c70be
N1ghtStorm <bushuevdv@mail.ru>
2021-11-05 14:31:02 +0300

    add metadata tests

--- src/tests/carbon_credits_tests.rs
+++ src/tests/carbon_credits_tests.rs
@@ -44,3 +44,48 @@ fn it_fails_for_create_cc_asset_not_owner_account() {
         assert_ne!(create_cc_result, Ok(()));
     });
 }
+
+#[test]
+fn it_works_for_set_cc_metadata() {
+    new_test_ext().execute_with(|| {
+        let (_, project_id, owner) = get_registerd_project_and_owner_gold_standard();
+        full_sign_annual_report_gold_standard();
+        let asset_id = 1;
+        let _ = CarbonCredits::create_carbon_credits(Origin::signed(owner), asset_id, owner, 1, project_id);
+        let set_metadata_result = CarbonCredits::set_carbon_credits_metadata(
+            Origin::signed(owner), 
+            asset_id, 
+            "CarbonToken".to_owned().as_bytes().to_vec(), 
+            "CT".to_owned().as_bytes().to_vec(), 
+            1
+        );
+        assert_ok!(set_metadata_result, ());
+    });
+}
+
+#[test]
+fn it_fails_for_set_empty_cc_metadata() {
+    new_test_ext().execute_with(|| {
+        let (_, project_id, owner) = get_registerd_project_and_owner_gold_standard();
+        full_sign_annual_report_gold_standard();
+        let asset_id = 1;
+        let _ = CarbonCredits::create_carbon_credits(Origin::signed(owner), asset_id, owner, 1, project_id);
+        let set_metadata_result_empty_name = CarbonCredits::set_carbon_credits_metadata(
+            Origin::signed(owner), 
+            asset_id, 
+            "".to_owned().as_bytes().to_vec(), 
+            "CT".to_owned().as_bytes().to_vec(), 
+            1
+        );
+
+        let set_metadata_result_empty_symbol = CarbonCredits::set_carbon_credits_metadata(
+            Origin::signed(owner), 
+            asset_id, 
+            "CarbonToken".to_owned().as_bytes().to_vec(), 
+            "".to_owned().as_bytes().to_vec(), 
+            1
+        );
+        assert_ne!(set_metadata_result_empty_name, Ok(()));
+        assert_ne!(set_metadata_result_empty_symbol, Ok(()));
+    });
+}
